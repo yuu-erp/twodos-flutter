d98050c070e9d72331912b3a089eb0b9d887c754
Lê Khải Hoàn <lekhaihoan@Macbook-M2-Pro-Yuu.local>
2024-03-11 17:14:28 +0700

    UPDATE

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_3/onboarding_page.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: OnboardingPage(),
+    );
+  }
+}
